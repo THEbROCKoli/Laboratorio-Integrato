fb6174c79ae4062cfc20dabda6f449c756e98389
THEbROCKoli <143262535+THEbROCKoli@users.noreply.github.com>
2023-12-13 11:45:27 +0100

    aggiunta libreria unity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.laboratorio_integrato"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.laboratorio_integrato"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation(files("../unityLib/unityLibrary.aar"))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Laboratorio Integrato"
 include(":app")
- 
